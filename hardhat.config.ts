1256f5e1b1ff16439a87293e45bdc02fa016842f
Morteza-Khedri <mkhedry3123@gmail.com>
2023-09-18 19:12:45 +0330

    remove extra libraries from lib folder

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,7 +15,7 @@ const config: HardhatUserConfig = {
         },
       },
       {
-        version: "0.8.19",
+        version: "0.8.20",
         settings: {
           optimizer: {
             enabled: true,
